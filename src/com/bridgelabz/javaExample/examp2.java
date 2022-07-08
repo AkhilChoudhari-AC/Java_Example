9451fc2df60050a25bfc15efa299b7853427b7b2
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-07-08 12:27:27 +0530

    ini

--- src/com/bridgelabz/javaExample/examp2.java
+++ src/com/bridgelabz/javaExample/examp2.java
@@ -3,5 +3,6 @@ package com.bridgelabz.javaExample;
 public class examp2 {
     public static void main(String[] args) {
         System.out.println("java example 2");
+        System.out.println("java java java");
     }
 }
